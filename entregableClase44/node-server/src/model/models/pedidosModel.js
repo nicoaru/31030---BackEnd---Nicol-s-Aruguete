048bbe08f061ab8a740caf7563d40a7b551a32be
Nico Aru <nico_aru@yahoo.com.ar>
2022-12-15 19:15:30 -0300

    entregable clase 44

---
+++ entregableClase44/node-server/src/model/models/pedidosModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const {Schema, model} = mongoose
+
+
+const PedidoSchema = new Schema({
+    userId: {type: String, required: true},
+    nombre: {type: String},
+    email: {type: String},
+    items: {type: Array, required: true, default: []},
+    precioTotal: {type: Number},
+    direccion: {type: String}
+}, {timestamps: true});
+
+
+
+  
+  
+const PedidosModel = model('pedidos', PedidoSchema);
+
+
+module.exports = {PedidosModel, PedidoSchema}
+
+
+// ver si se puede definir un schema para los productos dentro del array
